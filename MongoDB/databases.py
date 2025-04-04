137fd7ce5ea34194d1b70a526f46f704680ae48a
AlexeyShchukin <shchukin.developer@gmail.com>
2025-04-04 12:16:33 +0200

    MongoDB

---
+++ MongoDB/databases.py
@@ -0,0 +1,11 @@
+from connections import *
+
+db_ich = client.ich
+db_sample_data = client.sample_data
+# admin
+# config
+
+# ich_edit
+# local
+
+# test
---
+++ MongoDB/my_collections.py
@@ -0,0 +1,5 @@
+from databases import db_ich, db_sample_data
+
+customers = db_ich.customers
+trips = db_sample_data.trips
+spotify_youtube = db_ich.Spotify_Youtube
